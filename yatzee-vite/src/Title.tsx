d32809e793ce0bcaacdebd7d1a64ab449f0086f1
fudayear <amonnagasawa@gmail.com>
2024-11-19 05:50:11 +0000

    ある程度進めたが、バグが発生してる

---
+++ yatzee-vite/src/Title.tsx
@@ -0,0 +1,9 @@
+export const Title = () => {
+    const year: string = "584"
+    const season: string = "冷乾季"
+    return (
+        <div>
+            <h1>征代記第{year}年の{season}</h1>
+        </div>
+    )
+}
