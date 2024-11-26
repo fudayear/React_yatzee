b9f63855cfcf64c1876ee75575a4bad4c1820ca0
= <=>
2024-11-26 12:54:48 +0900

    依存関係のトラブルを解決するための変更

--- yatzee-vite/src/App.tsx
+++ yatzee-vite/src/App.tsx
@@ -1,11 +1,14 @@
-import { ChakraProvider, defaultSystem, Text } from '@chakra-ui/react'
+import {
+  ChakraProvider,
+  defaultSystem
+} from '@chakra-ui/react'
 import './App.css'
 
 function App() {
   return (
     <ChakraProvider value={defaultSystem}>
       <div>
-        <Text fontSize="2xl">家計簿アプリ</Text>
+
       </div>
     </ChakraProvider>
   )
