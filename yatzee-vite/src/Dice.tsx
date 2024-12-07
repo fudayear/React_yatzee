e963b654a191ea5d43ab6291772b11e4ee691552
= <=>
2024-12-07 14:46:59 +0900

    diceの動きの実装途中

--- yatzee-vite/src/Dice.tsx
+++ yatzee-vite/src/Dice.tsx
@@ -2,18 +2,29 @@ import { Box, ChakraProvider, defaultSystem, Image } from "@chakra-ui/react";
 
 type DiceProps = {
     value: number;
+    onDiceLock: () => void
 }
 
 const diceImages: { [key: number]: string } = {
+    1: '/images/dice-1.png',
+    2: '/images/dice-2.png',
+    3: '/images/dice-3.png',
+    4: '/images/dice-4.png',
+    5: '/images/dice-5.png',
+    6: '/images/dice-6.png',
+};
 
-}
 
-export default function Dice() {
+const Dice = ({ value, onDiceLock }: DiceProps) => {
+    let diceImage = diceImages[value] || '/images/dice-5.png';
+
     return (
         <ChakraProvider value={defaultSystem}>
-            <Box display="inline-block" border="1px solid" borderColor="gray.200" borderRadius="md">
+            <Box display="inline-block" border="1px solid" borderColor="gray.200" borderRadius="md" onClick={onDiceLock} >
                 <Image src={diceImage} alt={`Dice face with value ${value}`} boxSize="100px" />
             </Box>
         </ChakraProvider>
     )
 }
+
+export default Dice;
--- yatzee-vite/src/DiceCompornent.tsx
+++ yatzee-vite/src/DiceCompornent.tsx
@@ -1,11 +1,28 @@
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
+import { useState } from "react";
 import Dice from "./Dice";
 
+const diceValues: { [key: number]: number } = {
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0
+};
+
+const [diceNumber, setDiceNumber] = useState(0)
+
 export default function DiceCompornent() {
+
+    const diceLockNumber = (value: number) => {
+        setDiceNumber(value)
+    }
+
     return (
         <ChakraProvider value={defaultSystem}>
             <div>
-                <Dice />
+                <Dice onDiceLock={() => diceLockNumber} value={diceNumber} />
             </div>
         </ChakraProvider>
     )
